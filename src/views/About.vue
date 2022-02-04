51e9e49c5d27c5d4ed0952d9768dd7014f5e9687
CHOOPPERgm <harold-lego@live.com>
2022-02-04 17:45:15 -0600

    vueaxios

---
+++ src/views/About.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-if="result" class="about">
+ 
+    <h1>Wellcome to my web page about weather</h1>
+
+    <header>
+        <p>Tis is the weathr in {{ result.name }} to day</p>
+    </header>
+    
+    <img :src="'http://openweathermap.org/img/w/'+result.weather[0].icon+'.png'" alt=" " />
+    <p>{{ result.weather[0].description }}</p>
+    <p>Temperature: {{ result.main.temp }}</p>
+    <p>Degrees: {{ result.wind.deg }}</p>
+
+  </div>
+
+</template>
+
+
+<script>
+import axios from "axios";
+export default {
+  data: () => ({
+    result: null,
+  }),
+  created() {
+
+      axios.get("https://api.openweathermap.org/data/2.5/weather?q=playa%20del%20carmen&APPID=36ffda36a4d355a80a24f8d3bf89024e").then((result) => {
+        this.result = result.data;
+        console.log(result.data);
+      });
+  },
+};
+</script>
